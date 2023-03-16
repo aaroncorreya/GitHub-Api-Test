5cab3f470df907b5ee15da3e2b717ae8fd707b01
Aaron Correya <aaroncorreya@microsoft.com>
2023-03-16 10:30:09 -0700

    Added generated rules for expiration test

---
+++ script.ps1
@@ -0,0 +1,8 @@
+﻿$content = Get-Content -Path .\validtemplate.json -raw
+New-Item -ItemType Directory ".\GeneratedRules"
+
+for (($i = 0); $i -lt 1000; $i++)
+{ 
+    $newGuid = [System.Guid]::NewGuid()
+    $content.replace("{guid}", $newGuid) | Out-File ".\GeneratedRules\$($newGuid).json" -Encoding utf8
+}
