4420882c7872ea6e9805a86ed220b8dbfe970abb
Aaron Correya <aaroncorreya@microsoft.com>
2023-05-12 12:14:17 -0700

    Updated push function

--- .github/workflows/azure-sentinel-deploy-6475af99-6fa9-40fb-907a-465a5841e96f.ps1
+++ .github/workflows/azure-sentinel-deploy-6475af99-6fa9-40fb-907a-465a5841e96f.ps1
@@ -140,7 +140,8 @@ function PushCsvToRepo() {
     if ($resourceBranchExists -eq 0) {
         git switch --orphan $newResourceBranch
         git commit --allow-empty -m "Initial commit on orphan branch"
-        git push -u origin $newResourceBranch
+        # git push -u origin $newResourceBranch
+        git push -u "https://$githubAuthToken@github.com/$githubRepository.git" $newResourceBranch 
         New-Item -ItemType "directory" -Path ".sentinel"
     } else {
         git fetch > $null
@@ -150,7 +151,8 @@ function PushCsvToRepo() {
     Write-Output $content > $relativeCsvPath
     git add $relativeCsvPath
     git commit -m "Modified tracking table"
-    git push -u origin $newResourceBranch
+    # git push -u origin $newResourceBranch
+    git push -u "https://$githubAuthToken@github.com/$githubRepository.git" $newResourceBranch 
     git checkout $branchName
 }
 
@@ -560,7 +562,8 @@ function TryGetCsvFile {
         Remove-Item -Path $csvPath
         git add $csvPath
         git commit -m "Removed tracking file and moved to new sentinel created branch"
-        git push origin $branchName
+        # git push origin $branchName
+        git push "https://$githubAuthToken@github.com/$githubRepository.git" $branchName 
     }
 
     $relativeCsvPath = RelativePathWithBackslash $csvPath
